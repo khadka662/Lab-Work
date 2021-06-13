9479b6fdff0e9672895db2a74fd3ca11a7b10e8c
ASUS <khadkasagar290@gmail.com>
2021-06-13 14:09:31 +0545

    commit

---
+++ eight.py
@@ -0,0 +1,6 @@
+#Write a Python program which accepts the radius of a circle from the user and compute the area.
+# (area of circle = PI * r2)
+
+radius_circle = float(input('enter the radius of circle:'))
+area_circle = (3.14 * radius_circle**2 )
+print(f'the area of circle is {area_circle}')
---
+++ nine.py
@@ -0,0 +1,6 @@
+#Write a python program to find sum of the first n positive integers.
+#sum = (n*(n+1))/2
+num = int(input('Enter the positive integer:'))
+sum_n_positive_integers = (num*(num+1))/2
+print(sum_n_positive_integers)
+print("sum of first" ,num,"positive integers:" ,sum_n_positive_integers )
