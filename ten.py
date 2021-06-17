fcfe5392ddf6ca0b5d0da7c150d876428579a8d7
ASUS <khadkasagar290@gmail.com>
2021-06-17 19:14:48 +0545

    commit

--- ten.py
+++ ten.py
@@ -1,11 +1,14 @@
 #Write a Python program to convert seconds to day, hour, minutes and seconds.
+#Write a Python program to convert seconds to day, hour, minutes and seconds.
 
-time = float(input("Input time in seconds: "))
-day = time // (24 * 3600)
-time = time % (24 * 3600)
-hour = time // 3600
-time %= 3600
-minutes = time // 60
-time %= 60
-seconds = time
-print("d:h:m:s-> %d:%d:%d:%d" % (day, hour, minutes, seconds))
+seconds = float(input("Input time in seconds: "))
+minute = seconds/60
+hour = minute / 60
+day = hour/24
+print("d:h:m:s-> %d:%d:%d:%d" % (day, hour, minute, seconds))
+
+
+
+
+
+
